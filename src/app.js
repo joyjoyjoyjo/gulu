61d8f6afc5369bfc94dba1484e8ef583b0a38480
xiaoyiyi <xiaoyishikongde@163.com>
2019-07-05 23:30:19 +0800

    添加loading click

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ import Vue from "vue";
 import Button from './button'
 import Icon from './icon'
 Vue.component('g-button', Button);
-Vue.component('g-icon',Icon)
+Vue.component('g-icon',Icon);
 new Vue({
     el:'#app',
+    data:{
+        loading1:false,
+        loading2:true,
+        loading3:false,
+    }
 });
