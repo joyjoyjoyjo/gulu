d25b5deeb14dfcc0484b73aa3e68e2a2d2272243
xiaoyiyi <xiaoyishikongde@163.com>
2019-07-07 22:53:12 +0800

    使用chai.expect 添加4个测试用例

--- src/app.js
+++ src/app.js
@@ -13,3 +13,91 @@ new Vue({
         loading3:false,
     }
 });
+import chai from 'chai';
+
+const expect = chai.expect;
+//单元测试
+{
+    const  Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'set'
+        }
+    });
+    vm.$mount('#test');
+
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-set');
+    vm.$el.remove();
+    vm.$destroy()
+}
+{
+    const  Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'set',
+            loading:true,
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-loading');
+    vm.$el.remove();
+    vm.$destroy()
+}
+{
+    //测试order
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const  Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'set',
+            loading:true,
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq(`1`);
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    //测试order
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const  Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'set',
+            iconPosition:'right',
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq(`2`);
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    //mock
+    //测试click事件
+    const  Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'set',
+            iconPosition:'right',
+        }
+    });
+    vm.$mount();
+    vm.$on('click',function () {
+        console.log(1);
+    });
+
+    let button = vm.$el;
+    button.click();
+}
